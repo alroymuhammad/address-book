69c6d659cdf10184571c3a2ed056ffc080245d3c
alroymuhammad <alroymuhammad2@gmail.com>
2026-02-09 09:16:40 +0700

    feat: data as an array of objects contact

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+console.log("Hello World!");
+
+const contacts = [
+  {
+    id: 1,
+    firstName: "Steve",
+    lastName: "Jobs",
+    jobTitle: "CEO",
+    email: "stevejobs@apple.com",
+    phoneNumber: "+1278128318",
+    location: "San Francisco",
+  },
+  {
+    id: 2,
+    firstName: "Jude",
+    lastName: "Bellingham",
+    jobTitle: "Midfielder",
+    email: "jude.bellingham@realmadrid.com",
+    phoneNumber: "+1278128319",
+    location: "Madrid",
+  },
+];
+
+console.log(contacts);
