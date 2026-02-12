26db096a8baccda69cad2687e7405c5ee09938ca
alroymuhammad <alroymuhammad2@gmail.com>
2026-02-12 09:38:47 +0700

    feat: list contact and log contact function

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 "use strict";
-console.log("Hello World!");
+const logContact = (contacts) => {
+  if (!Array.isArray(contacts)) {
+    console.log("Provide an array of objects data structure");
+  } else if (contacts.length === 0) {
+    console.log("Your contact is empty");
+  }
+  contacts.forEach((contact) => {
+    console.log(
+      contact.firstName +
+        " " +
+        contact.lastName +
+        ", " +
+        contact.phoneNumber +
+        ", " +
+        contact.email,
+    );
+  });
+};
+const logContact2 = (contacts) => {
+  if (!Array.isArray(contacts)) {
+    console.log("Provide an array of objects data structure");
+  } else if (contacts.length === 0) {
+    console.log("Your contact is empty");
+  }
+  contacts.forEach((contact) => {
+    console.log(
+      `${contact.firstName} ${contact.lastName} (${contact.phoneNumber}) ${contact.email} in ${contact.location}`,
+    );
+  });
+};
+const listContact = (contacts) => {
+  logContact(contacts);
+  logContact2(contacts);
+};
 
 const contacts = [
   {
@@ -22,4 +55,5 @@ const contacts = [
   },
 ];
 
-console.log(contacts);
+// console.log(contacts);
+listContact(contacts);
