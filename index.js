d79284013327666883474c13816f60d9fac167fc
alroymuhammad <alroymuhammad2@gmail.com>
2026-02-24 11:25:14 +0700

    feat: modularize contact management functions and improve rendering logic

--- index.js
+++ index.js
@@ -1,127 +1,18 @@
 "use strict";
-const API_URL = "https://6995d3a3b081bc23e9c492b5.mockapi.io/api/contacts";
 
-const REQUIRED_FIELDS = [
-  "firstName",
-  "lastName",
-  "jobTitle",
-  "email",
-  "phoneNumber",
-  "location",
-];
+import {
+  loadContacts,
+  saveContacts,
+  fetchContactsFromAPI,
+  addContact,
+  deleteContact,
+  searchContacts,
+} from "./contact.js";
 
-const defaultContacts = [
-  {
-    id: 1,
-    firstName: "Steve",
-    lastName: "Jobs",
-    jobTitle: "CEO",
-    email: "stevejobs@apple.com",
-    phoneNumber: "+1278128318",
-    location: "San Francisco",
-  },
-  {
-    id: 2,
-    firstName: "Jude",
-    lastName: "Bellingham",
-    jobTitle: "Midfielder",
-    email: "jude.bellingham@realmadrid.com",
-    phoneNumber: "+1278128319",
-    location: "Madrid",
-  },
-];
-
-// TODO: modularize the files
-const saveContacts = (contacts) => {
-  localStorage.setItem("contacts", JSON.stringify(contacts));
-};
-
-const loadContacts = () => {
-  const stored = localStorage.getItem("contacts");
-  return stored ? JSON.parse(stored) : defaultContacts;
-};
-const fetchContactsFromAPI = async () => {
-  try {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    console.log("✅ Loaded from API:", data);
-    return data;
-  } catch (error) {
-    console.log(error);
-    return;
-  }
-};
-const logContacts = (contacts) => {
-  if (!Array.isArray(contacts)) {
-    console.log("Provide an array of objects data structure");
-    return;
-  } else if (contacts.length === 0) {
-    console.log("Your contact is empty");
-    return;
-  }
-  contacts.forEach((contact) => {
-    const fullName = `${contact.firstName} ${contact.lastName}`;
-    console.log(
-      `${fullName} | ${contact.jobTitle} | ${contact.phoneNumber} | ${contact.email}`,
-    );
-  });
-};
-
-const isValidContact = (contact) => {
-  return REQUIRED_FIELDS.every(
-    (field) => Object.keys(contact).includes(field) && contact[field] !== "",
-  );
-};
-
-const getNextId = (contacts) => {
-  if (contacts.length === 0) return 1;
-  return Math.max(...contacts.map((contact) => contact.id)) + 1;
-};
-
-const addContact = (contacts, newContact) => {
-  if (!isValidContact(newContact)) {
-    console.log("Invalid contact — missing required fields:", REQUIRED_FIELDS);
-    return contacts;
-  }
-  return [
-    ...contacts,
-    {
-      ...newContact,
-      id: getNextId(contacts),
-      createdAt: new Date().toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }),
-    },
-  ];
-};
-
-const updateContact = (contacts, updatedContact) => {
-  return contacts.map((contact) => {
-    if (contact.id === updatedContact.id) {
-      return { ...contact, ...updatedContact };
-    } else {
-      return contact;
-    }
-  });
-};
-
-const deleteContact = (contacts, id) => {
-  return contacts.filter((contact) => contact.id !== id);
-};
-
-const searchContacts = (contacts, query) => {
-  return contacts.filter(
-    (contact) =>
-      contact.firstName.toLowerCase().includes(query.toLowerCase()) ||
-      contact.lastName.toLowerCase().includes(query.toLowerCase()),
-  );
-};
-const containerElement = document.getElementById("container");
 const formElement = document.getElementById("search");
 const inputElement = document.getElementById("search-input");
 const listElement = document.getElementById("contacts-list");
+const contactFormElement = document.getElementById("contact-form");
 
 const renderList = (contactsList) => {
   listElement.innerHTML = "";
@@ -144,8 +35,6 @@ const renderList = (contactsList) => {
     <button class="edit-btn">Edit</button>
     <button class="delete-btn">Delete</button>
   `;
-
-    // Attach listeners AFTER innerHTML is set
     li.querySelector(".delete-btn").addEventListener("click", () => {
       contacts = deleteContact(contacts, contact.id);
       saveContacts(contacts);
@@ -154,18 +43,36 @@ const renderList = (contactsList) => {
 
     li.querySelector(".edit-btn").addEventListener("click", () => {
       console.log("Edit:", contact);
-      // your edit logic here
     });
 
     listElement.appendChild(li);
   });
 };
+
 let contacts = loadContacts();
 fetchContactsFromAPI();
 renderList(contacts);
+
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
   const query = inputElement.value;
   const results = searchContacts(contacts, query);
   renderList(results);
+});
+
+contactFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(contactFormElement);
+  const newContact = {
+    firstName: formData.get("firstName"),
+    lastName: formData.get("lastName"),
+    jobTitle: formData.get("jobTitle"),
+    email: formData.get("email"),
+    phoneNumber: formData.get("phoneNumber"),
+    location: formData.get("location"),
+  };
+  contacts = addContact(contacts, newContact);
+  saveContacts(contacts);
+  renderList(contacts);
+  contactFormElement.reset();
 });
